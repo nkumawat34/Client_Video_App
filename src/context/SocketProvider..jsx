f200484b94b9b4204abfe1268543e22b69b3774a
Neeraj Kumawat <42885320+nkumawat34@users.noreply.github.com>
2024-06-08 10:54:12 +0530

    Update SocketProvider..jsx

--- src/context/SocketProvider..jsx
+++ src/context/SocketProvider..jsx
@@ -8,7 +8,7 @@ export const useSocket=()=>{
 }
 export const  SocketProvider= (props)=>{
   
-  const socket=useMemo(()=>io('localhost:8000'),[])
+  const socket=useMemo(()=>io('https://backend-video-app.onrender.com'),[])
 
   return (
    <SocketContext.Provider value={socket}>
